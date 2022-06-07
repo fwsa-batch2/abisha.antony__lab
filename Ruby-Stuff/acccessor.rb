0b91b11c73f6920c5451416649f7470f1437b24a
abisha-antony <abishaantony.v@gmail.com>
2022-06-07 17:49:48 +0530

    module

---
+++ Ruby-Stuff/acccessor.rb
@@ -0,0 +1,7 @@
+class Access 
+     attr_accessor:name
+
+end
+ obj = Access.new
+ obj.name = "abi"
+ puts obj.name
--- Ruby-Stuff/module1.rb
+++ Ruby-Stuff/module1.rb
@@ -1,1 +1,9 @@
-
+module Mod1
+     def meth
+        for i in 1..9  do
+            puts i
+        end
+     end 
+end
+include Mod1
+Mod1::meth
